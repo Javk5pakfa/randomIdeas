3c5a10733a7b6112a241f7b4397f817118e6c454
Jack Hu <zxh496@miami.edu>
2024-02-15 18:26:26 -0500

    Work on thermo problem 3.18

---
+++ src/thermo_problem_318.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+NUp = np.linspace(100, 0, 101)
+Uen = np.linspace(-100, 100, 101)
+Mag = np.linspace(1, -1, 101)
+Omega = [math.factorial(100) / (math.factorial(x) * math.factorial(100 - x)) for x in NUp]
+Sentro = [math.log(x) for x in Omega]
+
+# print(Uen)
+
+# Find temperature.
+temperature = []
+i = 0
+while i < len(Omega):
+    if i == 0:
+        temperature.append(0)
+    elif 0 < i < len(Omega) - 1:
+        Udiff = Uen[i + 1] - Uen[i - 1]
+        Sdiff = Sentro[i + 1] - Sentro[i - 1]
+        temperature.append(Udiff / Sdiff)
+    elif i == len(Omega) - 1:
+        temperature.append(0)
+    i += 1
+
+print(temperature)
+
+# Find heat capacity.
