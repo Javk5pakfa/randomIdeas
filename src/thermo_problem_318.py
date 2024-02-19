3c4a6e760795644651a3a6c3a050c35125e3f5fa
Jack Hu <zxh496@miami.edu>
2024-02-18 22:17:43 -0500

    Add plots and configure file I/O

--- src/thermo_problem_318.py
+++ src/thermo_problem_318.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 NUp = np.linspace(100, 0, 101)
@@ -23,7 +22,7 @@ while i < len(Omega):
         if Sdiff != 0:
             temperature.append(Udiff / Sdiff)
         else:
-            temperature.append('NAN')
+            temperature.append(np.nan)
     elif i == len(Omega) - 1:
         temperature.append(0)
     i += 1
@@ -33,20 +32,20 @@ heat_capacity = []
 i = 0
 while i < len(Omega):
     if i == 0:
-        heat_capacity.append('NAN')
+        heat_capacity.append(np.nan)
     elif 0 < i < len(Omega) - 1:
         Udiff = Uen[i + 1] - Uen[i - 1]
-        if temperature[i + 1] != 'NAN' and temperature[i - 1] != 'NAN':
+        if temperature[i + 1] != np.nan and temperature[i - 1] != np.nan:
             Tdiff = temperature[i + 1] - temperature[i - 1]
         else:
-            Tdiff = 'NAN'
+            Tdiff = np.nan
 
-        if Tdiff != 'NAN' and temperature[i] != 'NAN':
+        if Tdiff != np.nan and math.isnan(temperature[i]) is False:
             heat_capacity.append(Udiff / Tdiff / 100)
         else:
-            heat_capacity.append('NAN')
+            heat_capacity.append(np.nan)
     elif i == len(Omega) - 1:
-        heat_capacity.append('NAN')
+        heat_capacity.append(np.nan)
     i += 1
 
 result_d = dict()
@@ -59,7 +58,34 @@ result_d['temperature'] = temperature
 result_d['heat_capacity'] = heat_capacity
 
 result_table = pd.DataFrame(data=result_d)
+# print(result_table.to_string())
 
-print(result_table)
+# Write to file?
+with open('table.txt', 'w') as fs:
+    fs.write(result_table.to_string())
 
 # Plotting various graphs.
+
+# Entropy vs. Energy.
+plt.subplot(2, 2, 1)
+plt.scatter(Uen, Sentro, marker='.')
+plt.title('Entropy vs. Energy')
+
+# Temperature vs. Energy.
+plt.subplot(2, 2, 2)
+plt.scatter(Uen, temperature, marker='.')
+plt.title('Temperature vs Energy')
+
+# Heat capacity vs. temperature.
+plt.subplot(2, 2, 3)
+plt.scatter(temperature, heat_capacity, marker='.')
+plt.title('Heat capacity vs. Temperature')
+
+# Magnetization vs. temperature.
+plt.subplot(2, 2, 4)
+plt.scatter(temperature, Mag, marker='.')
+plt.title('Magnetization vs. Temperature')
+
+plt.tight_layout()
+plt.savefig('thermo_problem_318.pdf')
+# plt.show()
