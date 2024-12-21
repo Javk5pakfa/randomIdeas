844c9af3a49992f99f09ed00798d947a1a041c77
Jack Hu <zxh496@miami.edu>
2024-12-21 12:59:06 +0800

    Clean pip download helper

--- src/pip_update_packages_helper.py
+++ src/pip_update_packages_helper.py
@@ -1,24 +1,32 @@
 # A helper tool for generating a pip install --update command with a large
-# amount of packages to be updated using a textfile of the list of packages
+# amount of packages to be updated using a text file of the list of packages
 # currently outdated.
 
 import pandas as pd
-import argparse as arg
+import argparse
+from pathlib import Path
 
-parser = arg.ArgumentParser(description='Parse pip text file.')
-parser.add_argument('--fn',
-                    type=str,
-                    help='The file path of the text file.',
-                    required=True,
-                    nargs=1)
+# Set up the argument parser
+parser = argparse.ArgumentParser(description='Parse pip text file.')
+parser.add_argument(
+    '--fn',
+    type=Path,
+    help='The file path of the text file.',
+    required=True
+)
 args = parser.parse_args()
 
-with open(args.fn[0], 'r') as file:
-    data = pd.read_table(file,
-                         sep='\s+',
-                         skiprows=2,
-                         header=None,
-                         names=["Package", "Version", "Latest", "Type"])
+# Read the file using pandas
+file_path = args.fn
+with file_path.open('r') as file:
+    data = pd.read_table(
+        file,
+        sep=r'\s+',
+        skiprows=2,
+        header=None,
+        names=["Package", "Version", "Latest", "Type"]
+    )
 
+# Generate the pip install command
 package_names = ' '.join(data['Package'].tolist())
-print('pip3 install --upgrade ' + package_names)
+print(f'pip3 install --upgrade {package_names}')
